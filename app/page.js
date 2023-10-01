755921238ace5934e5f4b18d92b70b6c0333d8de
Shaik Sohail Ahamed <sohailqwerty7@gmail.com>
2023-10-02 00:07:07 +0530

    added animated gradients

--- app/page.js
+++ app/page.js
@@ -1,19 +1,17 @@
-"use client";
-import TypingText1 from "./components/ui/TypingText1";
-import CursorAnimation from "./components/ui/CursorAnimation";
-
+import "./globals.css";
 export default function Home() {
   return (
     <>
-      <div className="main absolute top-0  w-screen h-screen z-10 flex items-center justify-center ">
-        <div className="flex gap-8 -z-10">
-          {/* <CursorAnimation /> */}
-          <h1 className=" tracking-tight bg-gradient-to-b from-[#FF72E1] to-[#F54C7A] bg-clip-text text-transparent text-9xl font-bold ">
+      <div className="w-screen h-screen  flex items-center justify-center ">
+        <div className="flex gap-8">
+          <h1 class=" text-9xl font-bold tracking-tight text-transparent">
             Portfolio
           </h1>
-          <TypingText1 />
-          <h1 className="text-slate-700 text-9xl font-bold ">
-            Aayega!
+          <h1 class=" text-9xl font-bold tracking-tight text-transparent">
+            Coming
+          </h1>
+          <h1 class="h-36 text-9xl font-bold tracking-tight text-transparent">
+            Soon
           </h1>
         </div>
       </div>
