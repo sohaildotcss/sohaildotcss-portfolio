9b917228de3f5c65376f8848ebbaeab0a9d56ea0
Shaik Sohail Ahamed <sohailqwerty7@gmail.com>
2023-10-02 00:13:37 +0530

    updated height to texts

--- app/page.js
+++ app/page.js
@@ -4,10 +4,10 @@ export default function Home() {
     <>
       <div className="w-screen h-screen  flex items-center justify-center ">
         <div className="flex gap-8">
-          <h1 class=" text-9xl font-bold tracking-tight text-transparent">
+          <h1 class="h-36 text-9xl font-bold tracking-tight text-transparent">
             Portfolio
           </h1>
-          <h1 class=" text-9xl font-bold tracking-tight text-transparent">
+          <h1 class="h-36 text-9xl font-bold tracking-tight text-transparent">
             Coming
           </h1>
           <h1 class="h-36 text-9xl font-bold tracking-tight text-transparent">
