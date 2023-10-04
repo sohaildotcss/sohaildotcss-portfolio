4b8e02a76316f6408580bbd471a4e65c3b6a4480
Shaik Sohail Ahamed <sohailqwerty7@gmail.com>
2023-10-04 14:29:25 +0530

    responsive to mobile

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,19 +1,19 @@
+"use client";
 import "../main.css";
+import Spline from "@splinetool/react-spline";
 
 const Main = () => {
   return (
     <>
-      <div className="w-screen h-screen  flex items-center justify-center ">
-        <div className="hero flex gap-8">
-          <h1 className="h-36 text-9xl font-bold tracking-tight text-transparent">
-            Portfolio
-          </h1>
-          <h1 className="h-36 text-9xl font-bold tracking-tight text-transparent">
-            Coming
-          </h1>
-          <h1 className="h-36 text-9xl font-bold tracking-tight text-transparent">
-            Soon
-          </h1>
+      <div className="w-full h-full lg:flex lg:items-center lg:justify-center flex justify-center items-center">
+        <Spline
+          className="absolute -z-20 lg:blur-lg opacity-60"
+          scene="https://prod.spline.design/1CNO6aUcbzQHSQsw/scene.splinecode"
+        />
+        <div className="hero lg:text-9xl h-36 text-7xl lg:flex justify-center items-center lg:gap-8 font-bold -tracking-wider text-transparent">
+          <h1 className="lg:w-[484px]">Portfolio</h1>
+          <h1 className="lg:h-36 h-20">Coming</h1>
+          <h1 className="text-center">Soon</h1>
         </div>
       </div>
     </>
